40b2c4e43ac039bddaf9aa88a64c01ab0e7e4ec0
Kristian <kristianmischke@gmail.com>
2018-06-12 23:35:19 -0400

    Added Book links/images and worked on book detail page

--- js/linkCorrection.js
+++ js/linkCorrection.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    const links = document.querySelectorAll('.post-content a');
+    const links = document.querySelectorAll('.post-content a, .mybook a');
     for(let i = 0; i < links.length; i++) {
         elem = links[i];
         elem.setAttribute("target", "_blank");
